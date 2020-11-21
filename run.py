cfa7d8d85860af477c7475ff64ba6b1f6bf65520
BenardBett <kiplangatbett5@gmail.com>
2020-11-22 01:05:34 +0300

    imported user from the user class and create a new user

---
+++ run.py
@@ -0,0 +1,10 @@
+from user import user
+    def create_user(username,password);
+        '''
+        function to create  new user
+        '''
+        new_user= User(username,password)
+        return new_user
+    
+   
+        
