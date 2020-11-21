ee037990b4d1bded9ff3f658774072275a8a1496
BenardBett <kiplangatbett5@gmail.com>
2020-11-22 01:30:46 +0300

    generated the main function with short codes

--- run.py
+++ run.py
@@ -29,3 +29,37 @@ from user import user
         function that checks if a user exist
         '''
         return User.user_exist(username)
+    
+    def display_all_users()
+        '''
+        function that returns all the saved users in the application
+        '''
+        return user.display= all_user()
+    def main():
+        print("Hi! welcome to an application that help you manage your credentials")
+        print("\n")
+        print("please use the following short codes to perform any task you want")
+        print("\n")
+        while True:
+            print("Use the short codes: cu- create a new user,du-display user, fu-find a user, ex-exist in the user list")
+            
+            short_code= input(), lower()
+            if short_code==cu:
+                print("New User")
+                print("_"*10)
+                print("username......")
+                username= input()
+                print("password......")
+                password=input
+                save_user(create_user(username,password))
+                print("\n")
+                print("New User{username}created")
+                print("\n")
+            else short_code=="du":
+                if display_all_users:
+                    print("This a list of all saved users")
+                    print("\n")
+                    
+                    for user in display_all_users():
+                        print(fu*{user.username})
+                        print("\n")
