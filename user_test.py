5e6f5e234a107b4785f94371dff98907e7277a68
BenardBett <kiplangatbett5@gmail.com>
2020-11-21 15:05:40 +0300

    created a test for deleting multiple saved users

--- user_test.py
+++ user_test.py
@@ -20,7 +20,7 @@ class TestUser(unittest.TestCase):
         '''
         tearDown method that does clean up after each test case has run.
         '''
-        Contact.user_list = []
+        User.user_list = []
         
 
     def test_init(self):
@@ -38,6 +38,19 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
+
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()  
+        test_user = User("Test","user") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+  
+
 
 if __name__ == '__main__':
     unittest.main()
